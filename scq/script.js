abb92ecfc418ebe8490f97657a05e46c248bf6b0
Joancarles Casasín <joanca@casasin.com>
2023-10-10 15:17:15 +0200

    Skip publishing old events

--- scq/script.js
+++ scq/script.js
@@ -9,21 +9,23 @@ http.onload = function(){
 		let events = JSON.parse(this.responseText);
 		let output = "";
 		for(let item of events){
-			output += `
-					<div class="container-grid">
-						<div class="col col-left">
-							<p class="dia_text">${item.dia_text}</p>
-							<p class="dia">${item.dia}</p>
-							<p class="mes">${item.mes}</p>
+			if(item.publicar == "TRUE"){
+				output += `
+						<div class="container-grid">
+							<div class="col col-left">
+								<p class="dia_text">${item.dia_text}</p>
+								<p class="dia">${item.dia}</p>
+								<p class="mes">${item.mes}</p>
+							</div>
+							<div class="col col-right">
+								<p class="banda">${item.banda}</p>
+								<p class="sala">${item.sala}</p>
+								<p class="poblacio"> ${item.poblacio}</p>
+								<p class="hora">${item.hora} h</p>
+							</div>
 						</div>
-						<div class="col col-right">
-							<p class="banda">${item.banda}</p>
-							<p class="sala">${item.sala}</p>
-							<p class="poblacio"> ${item.poblacio}</p>
-							<p class="hora">${item.hora} h</p>
-						</div>
-					</div>
-			`;
+				`;
+			}
 		}
 
 		document.querySelector(".events").innerHTML = output;
